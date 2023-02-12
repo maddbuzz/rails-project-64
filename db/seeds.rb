515bee2c012b1c9555479b6a6e08dea22f5778c2
Andrey Anisimov <maddbuzz@gmail.com>
2023-02-12 20:21:35 +0800

    refactor seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Category.delete_all
-categories = Category.create([{ name: 'Категория 1' }, { name: 'Категория 2' }])
+Category.create([{ name: 'Категория 1' }, { name: 'Категория 2' }]) unless Category.count
 
-User.create email: 'john@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create(email: 'john@gmail.com', password: 'topsecret', password_confirmation: 'topsecret') unless User.count
 users = User.all
 
-Post.delete_all
-10.times do
-  Post.create({ title: Faker::Lorem.unique.sentence, body: Faker::Lorem.unique.paragraph, category: categories.sample, creator: users.sample })
+unless Post.count
+  10.times do
+    Post.create({ title: Faker::Lorem.unique.sentence, body: Faker::Lorem.unique.paragraph, category: categories.sample, creator: users.sample })
+  end
 end
